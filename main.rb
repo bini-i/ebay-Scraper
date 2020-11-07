f067f9e99b69f436981e78c4d0ff3b0993f4f59a
bini-i <binyam.sh1@gmail.com>
2020-11-07 14:32:27 +0300

    Add scraper method
    - takes url as argument and return an array of filterd content

--- main.rb
+++ main.rb
@@ -8,6 +8,12 @@ def fetch (url)
     Nokogiri::HTML(open(url))
 end
 
+def scraper(url)
+    doc =fetch(url)
+
+    products_array = doc.css('#mainContent #srp-river-results .s-item__info');
+end
+
 url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=iphone+11&_sacat=0"
 
-p fetch(url)
+p scraper(url)
