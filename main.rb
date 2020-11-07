477d4919e178d56ea3f00f6fa5bf3eba27a012d2
bini-i <binyam.sh1@gmail.com>
2020-11-07 14:29:54 +0300

    Add fetch method
    - takes url as argument and returns nokogiri object created using the
    url

--- main.rb
+++ main.rb
@@ -4,3 +4,10 @@ require 'nokogiri'
 require 'httparty'
 require 'open-uri'
 
+def fetch (url) 
+    Nokogiri::HTML(open(url))
+end
+
+url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=iphone+11&_sacat=0"
+
+p fetch(url)
