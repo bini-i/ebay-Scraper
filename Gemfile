c069ddef85084e5c891bf5a95815c47224ea4663
bini-i <binyam.sh1@gmail.com>
2020-11-07 16:21:44 +0300

    Fix styling errors in main.rb

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'httparty'
 gem 'nokogiri'
 gem 'open-uri'
-gem 'httparty'
+gem 'rubocop', '~>0.81.0'
--- main.rb
+++ main.rb
@@ -4,52 +4,52 @@ require 'nokogiri'
 require 'httparty'
 require 'open-uri'
 
-def fetch (url) 
-    Nokogiri::HTML(open(url))
+def fetch(url)
+  Nokogiri::HTML(URI.open(url))
 end
 
 def scraper(url)
-    doc = fetch(url)
+  doc = fetch(url)
 
-    doc.css('#mainContent #srp-river-results .s-item__info');
+  doc.css('#mainContent #srp-river-results .s-item__info')
 end
 
 def display_product(doc)
-    doc.each_with_index do |item, indx|
-        puts "*********************"
-        # product name
-        product_name = item.at_css('.s-item__title').text
-        puts "#{indx} + product name = #{product_name}"
-        
-        # condition
-        condition = item.at_css('.SECONDARY_INFO').text
-        puts "condition = #{condition}"
-        
-        # price
-        price = item.at_css('.s-item__price').text
-        puts "price = #{price}"
-        
-        # bid count
-        bid_count = item.at_css('.s-item__bidCount')
-        bid_count = bid_count.text unless bid_count.nil?
-        puts "bid count = #{bid_count}"
-        
-        # bid time left
-        bid_time_left = item.at_css('.s-item__time-left')
-        bid_time_left = bid_time_left.text unless bid_time_left.nil?
-        puts "bid time left = #{bid_time_left}"
-        
-        # location
-        location = item.at_css('.s-item__location').text
-        puts "location = #{location}"
-        
-        # shipping cost
-        shipping_cost = item.at_css('.s-item__shipping').text
-        puts "shipping cost = #{shipping_cost}"
-    end
+  doc.each_with_index do |item, indx|
+    puts '*********************'
+    # product name
+    product_name = item.at_css('.s-item__title').text
+    puts "#{indx} + product name = #{product_name}"
+
+    # condition
+    condition = item.at_css('.SECONDARY_INFO').text
+    puts "condition = #{condition}"
+
+    # price
+    price = item.at_css('.s-item__price').text
+    puts "price = #{price}"
+
+    # bid count
+    bid_count = item.at_css('.s-item__bidCount')
+    bid_count = bid_count.text unless bid_count.nil?
+    puts "bid count = #{bid_count}"
+
+    # bid time left
+    bid_time_left = item.at_css('.s-item__time-left')
+    bid_time_left = bid_time_left.text unless bid_time_left.nil?
+    puts "bid time left = #{bid_time_left}"
+
+    # location
+    location = item.at_css('.s-item__location').text
+    puts "location = #{location}"
+
+    # shipping cost
+    shipping_cost = item.at_css('.s-item__shipping').text
+    puts "shipping cost = #{shipping_cost}"
+  end
 end
 
-url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=iphone+11&_sacat=0"
+url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=iphone+11&_sacat=0'
 
 doc = scraper(url)
 
