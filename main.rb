8c1ff2c0b2d98ef6af9d58ed984b812d2fd90130
bini-i <binyam.sh1@gmail.com>
2020-11-08 13:43:10 +0300

    Update post method in main.rb
    - grabs value from input field in index.erb
    - concatenate to base url to form complete url string

--- main.rb
+++ main.rb
@@ -15,50 +15,8 @@ def scraper(url)
   doc.css('#mainContent #srp-river-results .s-item')
 end
 
-def display_product(doc)
-  doc.each_with_index do |item, indx|
-    puts '*********************'
-    # product name
-    product_name = item.at_css('.s-item__title').text
-    puts "#{indx} + product name = #{product_name}"
-
-    # condition
-    condition = item.at_css('.SECONDARY_INFO').text
-    puts "condition = #{condition}"
-
-    # price
-    price = item.at_css('.s-item__price').text
-    puts "price = #{price}"
-
-    # bid count
-    bid_count = item.at_css('.s-item__bidCount')
-    bid_count = bid_count.text unless bid_count.nil?
-    puts "bid count = #{bid_count}"
-
-    # bid time left
-    bid_time_left = item.at_css('.s-item__time-left')
-    bid_time_left = bid_time_left.text unless bid_time_left.nil?
-    puts "bid time left = #{bid_time_left}"
-
-    # location
-    location = item.at_css('.s-item__location').text
-    puts "location = #{location}"
-
-    # shipping cost
-    shipping_cost = item.at_css('.s-item__shipping').text
-    puts "shipping cost = #{shipping_cost}"
-  end
-end
-
-url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=iphone+11&_sacat=0'
-
-# doc = scraper(url)
-
-# puts doc.count
-
-# gets
-
-# display_product(doc)
+url_left = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw='
+url_right = '&_sacat=0'
 
 # routes
 get '/' do
@@ -66,6 +24,10 @@ get '/' do
 end
 
 post '/search' do
+    product_name = params['product_name']
+    arr = product_name.split()
+    product_name = arr.join('+')
+    url = url_left + product_name + url_right
     @products = scraper(url)
     erb :search_result
 end
